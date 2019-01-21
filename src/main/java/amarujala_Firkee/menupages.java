bb19adb825cec334ca431fe7515ebe519728473e
akhilesh <akhilesh@akhilesh-Lenovo-B40-70>
2019-01-21 13:41:41 +0530

    3rd commit

--- src/main/java/amarujala_Firkee/menupages.java
+++ src/main/java/amarujala_Firkee/menupages.java
@@ -5,10 +5,40 @@
  */
 package amarujala_Firkee;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 /**
  *
  * @author akhilesh
  */
 public class menupages {
     
+    private static WebElement element = null;
+    
+    public static WebElement firkee_cat_0(WebDriver driver){
+        element = driver.findElement(By.xpath("/html/body/div[4]/div/div/div/nav/ul/li[1]/span"));
+        return element;
+    }
+     
+    public static WebElement firkee_cat_1(WebDriver driver){
+        element = driver.findElement(By.xpath("/html/body/div[4]/div/div/div/nav/ul/li[1]/span"));
+        return element;
+    }
+    
+    public static WebElement firkee_cat_2(WebDriver driver){
+        element = driver.findElement(By.xpath("/html/body/div[4]/div/div/div/nav/ul/li[1]/span"));
+        return element;
+    }
+    
+    public static WebElement firkee_cat_3(WebDriver driver){
+        element = driver.findElement(By.xpath("/html/body/div[4]/div/div/div/nav/ul/li[1]/span"));
+        return element;
+    }
+    
+    public static WebElement firkee_cat_4(WebDriver driver){
+        element = driver.findElement(By.xpath("/html/body/div[4]/div/div/div/nav/ul/li[1]/span"));
+        return element;
+    }
 }
